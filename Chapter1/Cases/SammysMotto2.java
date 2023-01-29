74df8fd8a7ce087448fc02abd9e106602f6c9c20
Jan <72297663+jinxjan@users.noreply.github.com>
2023-01-29 03:07:56 -0500

    Delete Chapter1 directory

--- Chapter1/Cases/SammysMotto2.java
+++
@@ -1,9 +0,0 @@
-public class SammysMotto2 {
-   public static void main(String[] args) {
-      System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-      System.out.println("S                                    S");
-      System.out.println("S  Sammy's makes it fun in the sun.  S");
-      System.out.println("S                                    S");
-      System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
-   }
-}
--- Chapter1/README.txt
+++
@@ -1,1 +0,0 @@
-I did not save anything besides my case files until chapter 3 when I started to put my files on here.
